cc9b4e834a6784c380a82c1449f70c76c3aa8923
Asadbek <a.nosirjonov@uic.group>
2022-07-30 23:08:46 +0500

    fix

--- src/Pages/Outer-tourism-slug/slug.jsx
+++ src/Pages/Outer-tourism-slug/slug.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ReactStars from "react-rating-stars-component";
 import Slider from "react-slick";
-import "../../styles/GlobalStyle.styled";
 // import { SliderImgWrapper } from "../../Home/HomeStyle.styled";
 import {
   GlobalContainer,
   InnerAndOuterContainer,
-  WrapperNavbar,
 } from "../../styles/GlobalStyle.styled";
 import img from "../../assets/photo/sma.jpg";
 import img2 from "../../assets/photo/card.jpg";
@@ -26,6 +24,7 @@ const OuterTourismSlug = () => {
   useEffect(() => {
     window.addEventListener("click", (e) => {
       if (e.target.id === "close") {
+        // console.log(e.target.id);
         setIsActiveModal(false);
         document.body.style.overflow = "visible";
       }
@@ -98,9 +97,7 @@ const OuterTourismSlug = () => {
         <GlobalContainer>
           <h1>Maqsad</h1>
         </GlobalContainer>
-        <WrapperNavbar >
-          {isActiveModal && <Modal close={closeModal} />}
-        </WrapperNavbar>
+        {isActiveModal && <Modal close={closeModal} />}
       </InnerAndOuterContainer>
       <GlobalContainer>
         <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-3 lg:gap-7 md:gap-7 gap-0 py-[50px]">
--- src/styles/GlobalStyle.styled.jsx
+++ src/styles/GlobalStyle.styled.jsx
@@ -14,8 +14,9 @@ export const GlobalContainer = styled.div`
 
 export const WrapperNavbar = styled.div`
   position: absolute;
+  display: block;
   width: 100%;
-  z-index: ${({ active }) => (active ? "-1" : "1")};
+  z-index: 999;
 
   .sticky_nav {
     transition: all 0.5s ease;
@@ -27,6 +28,9 @@ export const WrapperNavbar = styled.div`
     justify-content: space-between;
     padding: 5px 0;
     background-color: #eee;
+    ul {
+      // margin-right: 70px;
+    }
     li {
       color: #223645;
     }
@@ -39,10 +43,6 @@ export const WrapperNavbar = styled.div`
     }
   }
 `;
-
-// export const ModalDiv = styled.div`
-
-// `
 
 export const TopNavbar = styled.div`
   background-color: transparent;
@@ -79,6 +79,7 @@ export const InnerAndOuterContainer = styled.div`
   text-transform: uppercase;
   margin-bottom: 0;
   opacity: 0.9;
+
   h1 {
     font-size: 60px;
     font-weight: 800;
