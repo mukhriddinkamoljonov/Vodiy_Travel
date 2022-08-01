9c40c6638cafecc351cc3c167463d27f98a18f9c
Asadbek <a.nosirjonov@uic.group>
2022-08-01 12:40:52 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import PropagateLoader from "react-spinners/PropagateLoader";
 import Main from "./Layout/Main";
 import Routers from "./Router/Routers";
 import { GlobalStyle } from "./styles/GlobalStyle.styled";
+
 function App() {
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, []);
+
   return (
     <>
       {loading ? (
